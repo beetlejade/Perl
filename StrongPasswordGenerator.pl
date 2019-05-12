94d9b07c9dee609df38de27107c23f9af37da87b
Ilian Riveiro <50547564+beetlejade@users.noreply.github.com>
2019-05-12 19:23:21 +0200

    Update 1
    The user can now choose what constitutes his password (symbols, numbers..)

--- StrongPasswordGenerator.pl
+++ StrongPasswordGenerator.pl
@@ -1,17 +1,67 @@
 #!/usr/bin/perl 
 
-use warnings;
+# Variables--
+#--------------------------------------
+
+my $choice;
+my $passize;
+my $nchar;
+my $genpass;
+my @char = ();
+
+#---------------------------------------
+
+# Choice Function (Yes/No) --
+#--------------------------------------
+
+sub choice() {
+        my $a = @_;
+        chomp(my $input = <STDIN>);
+
+        if ($input =~ /^[Y]?$/i) {
+                $a =1;
+        }
+        elsif ($input =~ /^[N]$/i) {
+                $a =0;
+        }
+        else {
+                print("please try again\n");
+                choice($a);
+        }
+}
+
+#--------------------------------------
+
+# Inputs of user's wills. --
+#--------------------------------------
 
 print("Password length ?\n");
-my $passize = <STDIN>;
-my $password;
+$passize = <STDIN>;
 
-my @char = (('A'..'Z'), ('a'..'z'), ('!','@','%','^'), (0..9));
-my $nchar = $#char + 1;
+print "Include Symbols ? ('!','@','%','^') (Y/n)\n";
+$choice = choice();
+push (@char,'!','@','%','^') if($choice);
+print "Include Lowercase Characters ? (Y/n)\n";
+$choice = choice();
+push (@char,(a..z)) if($choice);
+print "Include Uppercase Characters ? (Y/n)\n";
+$choice = choice();
+push (@char,(A..Z)) if($choice);
+print "Include Numbers ?(Y/n)\n";
+$choice = choice();
+push (@char,(0..9)) if($choice);
+
+#--------------------------------------
+
+#Creating the random password --
+#--------------------------------------
+
+$nchar = $#char + 1;
 
 for(1..$passize) {
         $genpass .= $char[int(rand($nchar))];
 }
 
-print "$genpass\n";
+#--------------------------------------
 
+print "$genpass\n";
